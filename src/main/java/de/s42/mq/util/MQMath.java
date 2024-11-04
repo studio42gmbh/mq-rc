2d6f70bf25366bf29964cc63d264d84d45aaf605
Benjamin Schiller <bs@s42m.de>
2024-11-04 23:10:42 +0100

    Added lerp for vectors

--- src/main/java/de/s42/mq/util/MQMath.java
+++ src/main/java/de/s42/mq/util/MQMath.java
@@ -1,15 +1,18 @@
 /*
  * Copyright Studio 42 GmbH 2021. All rights reserved.
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *  
+ *
  * For details to the License read https://www.s42m.de/license
  */
 package de.s42.mq.util;
+
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 /**
  *
@@ -17,6 +20,7 @@ package de.s42.mq.util;
  */
 public final class MQMath
 {
+
 	public static final float EPSILON = 0.000001f;
 	public static final float ONE_MINUS_EPSILON = 1.0f - EPSILON;
 	public static final float ONE_PLUS_EPSILON = 1.0f + EPSILON;
@@ -43,6 +47,23 @@ public final class MQMath
 	public static float lerp(float value, float target, float t)
 	{
 		return value + (target - value) * t;
+	}
+
+	public static Vector2f lerp(Vector2f value, Vector2f target, float t)
+	{
+		return new Vector2f(
+			value.x + (target.x - value.x) * t,
+			value.y + (target.y - value.y) * t
+		);
+	}
+
+	public static Vector3f lerp(Vector3f value, Vector3f target, float t)
+	{
+		return new Vector3f(
+			value.x + (target.x - value.x) * t,
+			value.y + (target.y - value.y) * t,
+			value.z + (target.z - value.z) * t
+		);
 	}
 
 	public static float toRadians(float degrees)
